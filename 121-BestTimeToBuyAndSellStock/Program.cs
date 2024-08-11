67c3c2c761e944e124ef579259a7995cb9685f9c
Robin Landerloos <rlanderloos@gmail.be>
2024-08-11 20:43:52 +0200

    Finished 121 Best Time to buy and sell stoc

---
+++ 121-BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DebugHelpers;
+
+Console.WriteLine("121 - Best Time to Buy and Sell Stock");
+
+MaxProfit([7, 1, 5, 3, 6, 4]).Print();
+MaxProfit([7, 6, 4, 3, 1]).Print();
+
+return;
+
+int MaxProfit(int[] prices)
+{
+	var maxProfit = 0;
+	var lowestPrice = int.MaxValue;
+
+	foreach (var currentPrice in prices)
+	{
+		if (currentPrice < lowestPrice)
+		{
+			lowestPrice = currentPrice;
+			continue;
+		}
+		if(currentPrice - lowestPrice > maxProfit)
+		{
+			maxProfit = currentPrice - lowestPrice;
+		}
+	}
+
+	return maxProfit;
+}
