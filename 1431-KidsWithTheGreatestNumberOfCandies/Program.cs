ef3e9bb6bcc72e552cb93c40d365338b26501257
Robin Landerloos <rlanderloos@gmail.be>
2024-08-17 17:39:39 +0200

    Finished 1431

---
+++ 1431-KidsWithTheGreatestNumberOfCandies/Program.cs
@@ -0,0 +1,24 @@
+﻿using DebugHelpers;
+
+Console.WriteLine("1431 - Kids With the Greatest Number of Candies");
+
+KidsWithCandies([2, 3, 5, 1, 3], 3).ToArray().Print();
+KidsWithCandies([4,2,1,1,2], 1).ToArray().Print();
+KidsWithCandies([12, 1, 12], 10).ToArray().Print();
+
+IList<bool> KidsWithCandies(int[] candies,
+	int extraCandies)
+{
+	var largest = candies.Max();
+
+	var result = new bool[candies.Length];
+	for (var i = 0; i < candies.Length; i++)
+	{
+		if (candies[i] + extraCandies >= largest)
+		{
+			result[i] = true;
+		}
+	}
+
+	return result;
+}
