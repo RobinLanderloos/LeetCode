d9cf72e770fe55cc0f2ef2482038a41c3877182f
rlanderloos <rlanderloos@sbssolutions.be>
2024-09-09 09:56:08 +0200

    Finished 746 - Min cost climbing stairs
    With help

---
+++ 746-MinCostClimbingStairs/Stair.cs
@@ -0,0 +1,23 @@
+﻿namespace _746_MinCostClimbingStairs;
+
+public class Stair(int[] cost)
+{
+	// ReSharper disable once InconsistentNaming
+
+
+	public int MinimumCostToClimb()
+	{
+		var dp = new int[cost.Length + 1];
+
+		dp[0] = cost[0];
+		dp[1] = cost[1];
+
+		for (var i = 2; i <= cost.Length; i++)
+		{
+			var current = i == cost.Length ? 0 : cost[i];
+			dp[i] = current + Math.Min(dp[i - 1], dp[i - 2]);
+		}
+
+		return dp[cost.Length];
+	}
+}
---
+++ 746-MinCostClimbingStairs/StairTests.cs
@@ -0,0 +1,18 @@
+﻿using FluentAssertions;
+
+namespace _746_MinCostClimbingStairs;
+
+public class StairTests
+{
+	[TestCase(new[] { 10, 15, 20 }, 15)]
+	[TestCase(new[] { 1, 100, 1, 1, 1, 100, 1, 1, 100, 1 }, 6)]
+	public void ShouldGiveMinimumCost(int[] cost,
+		int expectedCost)
+	{
+		var stair = new Stair(cost);
+
+		var minCost = stair.MinimumCostToClimb();
+
+		minCost.Should().Be(expectedCost);
+	}
+}
