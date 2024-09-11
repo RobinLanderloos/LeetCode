44042d160bd0a13c6cc80844592e063ae2bc8f50
rlanderloos <rlanderloos@sbssolutions.be>
2024-09-11 10:02:18 +0200

    Finished 151 - Reverse Words In a String

---
+++ 151-ReverseWordsInAString/StringReverser.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace _151_ReverseWordsInAString;
+
+public static class StringReverser
+{
+	public static string ReverseWords(string s)
+	{
+		var words = new List<string>();
+
+		var word = new StringBuilder();
+		foreach (var character in s)
+		{
+			if (character != ' ')
+			{
+				word.Append(character);
+			}
+			else
+			{
+				if (word.Length > 0)
+				{
+					words.Add(word.ToString());
+					word.Clear();
+				}
+			}
+		}
+
+		if (word.Length > 0)
+		{
+			words.Add(word.ToString());
+		}
+
+		var reversedBuilder = new StringBuilder();
+		for (int i = words.Count - 1; i >= 0; i--)
+		{
+			reversedBuilder.Append(words[i]);
+			if (i != 0)
+			{
+				reversedBuilder.Append(' ');
+			}
+		}
+
+		return reversedBuilder.ToString();
+	}
+}
---
+++ 151-ReverseWordsInAString/StringReverserTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+
+namespace _151_ReverseWordsInAString;
+
+public class StringReverserTests
+{
+	[TestCase("the sky is blue", "blue is sky the")]
+	[TestCase("  hello world  ", "world hello")]
+	[TestCase("a good  example", "example good a")]
+	public void Should_ReverseWords_InString(string input, string expectedOutput)
+	{
+		var reversed = StringReverser.ReverseWords(input);
+
+		reversed.Should().Be(expectedOutput);
+	}
+}
